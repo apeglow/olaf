0cef9424b716272cdf6ecdba3368ae7096dc8880
André Peglow <andre@andre-laptop.(none)>
2011-02-19 13:21:52 +0100

    first class

---
+++ src/main/java/de/mobile/olaf/Main.java
@@ -0,0 +1,8 @@
+package de.mobile.olaf;
+
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("Hello World");
+                }
+                }
+                
