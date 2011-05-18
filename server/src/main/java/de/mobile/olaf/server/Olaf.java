3d59107ac5d8541074051881ef11bcd00ee42f37
Holger Hammel <hhammel@team.mobile.de>
2011-05-18 19:09:46 +0200

    Added netty websocket server, first step: handshake and echo (see netty websocket example ;)

--- server/src/main/java/de/mobile/olaf/server/Olaf.java
+++ server/src/main/java/de/mobile/olaf/server/Olaf.java
@@ -20,94 +20,99 @@ import de.mobile.olaf.server.esper.eventlistener.external.IpAddressUsedForContac
 import de.mobile.olaf.server.esper.eventlistener.external.IpAddressUsedForPostingInDifferentCountriesEventListener;
 import de.mobile.olaf.server.esper.eventlistener.external.IpAddressUsedInDifferentCountriesEventListener;
 
+
 /**
  * The OLAF application.
  * 
  * @author andre
- *
+ * 
  */
 public class Olaf {
-	
-	/**
-	 * This map will be stored either in a database or a config file later.
-	 */
-	public final static List<PartnerSite> ipAddress2SiteMap = Arrays.asList(
-			new PartnerSite("mobile.de", Country.DE, PartnerSiteType.MOTORS_CLASSIFIED, PartnerNotifierType.REST, URI.create("http://localhost:8080/olaf_rest_service")),
-			new PartnerSite("marktplaats", Country.NL, PartnerSiteType.GENERAL_CLASSIFIED, PartnerNotifierType.REST, URI.create("http://localhost:8080/olaf_rest_service")),
-			new PartnerSite("annunci", Country.IT, PartnerSiteType.GENERAL_CLASSIFIED, PartnerNotifierType.REST, URI.create("http://localhost:8080/olaf_rest_service")));//
-	
-	
-	private final IpAddressUsageNotificationService ipAddressUsageNotificationService;
-	
-	private final PartnersNotificationService partnersNotificationService;
-	
-	private OlafTcpServer server;  
-	
-	public static void main(String[] args) throws IOException {
-		Olaf olaf = new Olaf();
-		olaf.start();
-	}
-	
-	public Olaf() {
-		EPServiceProvider epServiceProvider = EPServiceProviderManager.getDefaultProvider();
-		partnersNotificationService = new PartnersNotificationService(ipAddress2SiteMap);
-		ipAddressUsageNotificationService = new IpAddressUsageNotificationService(epServiceProvider);
-		
-		epServiceProvider //
-			.getEPAdministrator() //
-			.createEPL("create window RatedIpAddressWindow.win:time(360 min) as " +
-					   "select * from " + RatedIpAddress.class.getName());
-		
-		epServiceProvider //
-			.getEPAdministrator() //
-			.createEPL("insert into RatedIpAddressWindow select * from " + RatedIpAddress.class.getName());
-		
-		/*
-		 * listen to ip address rated events
-		 */
-		//IpAddressRatedEventListener.register(epServiceProvider, partnersNotificationService);
-		
-		/*
-		 * Listen to ip usage in different countries events
-		 */
-		IpAddressUsedInDifferentCountriesEventListener.register(epServiceProvider);
-		
-		/*
-		 * Listen to fraud announcments
-		 */
-		IpAddressAnnouncedAsFraudEventListener.register(epServiceProvider);
-		
-		/*
-		 * Listen to contact events in different countries 
-		 */
-		IpAddressUsedForContactInDifferentCountriesEventListener.register(epServiceProvider);
-		
-		/*
-		 *  used excessively
-		 */
-		IpAddressUsedForContactExcessivelyEventListener.register(epServiceProvider);
-		
-		/*
-		 * used for posting in different countries
-		 */
-		IpAddressUsedForPostingInDifferentCountriesEventListener.register(epServiceProvider);
-	}
-	
-	
-	/**
-	 * Starts the service.
-	 * 
-	 * @throws IOException
-	 */
-	public void start() throws IOException {
-		server = new OlafTcpServer(5555, ipAddressUsageNotificationService);
-	}
-	
-	
-	public void stop() {
-	    server.close();
-		partnersNotificationService.shutdown();
-	}
-	
+
+    /**
+     * This map will be stored either in a database or a config file later.
+     */
+    public final static List<PartnerSite> ipAddress2SiteMap = Arrays.asList(
+        new PartnerSite("mobile.de", Country.DE, PartnerSiteType.MOTORS_CLASSIFIED, PartnerNotifierType.REST, URI
+                .create("http://localhost:8080/olaf_rest_service")),
+        new PartnerSite("marktplaats", Country.NL, PartnerSiteType.GENERAL_CLASSIFIED, PartnerNotifierType.REST, URI
+                .create("http://localhost:8080/olaf_rest_service")),
+        new PartnerSite("annunci", Country.IT, PartnerSiteType.GENERAL_CLASSIFIED, PartnerNotifierType.REST, URI
+                .create("http://localhost:8080/olaf_rest_service")));//
+
+    private final IpAddressUsageNotificationService ipAddressUsageNotificationService;
+
+    private final PartnersNotificationService partnersNotificationService;
+
+    private OlafTcpServer server;
+
+    private OlafWebsocketServer websocketServer;
+
+    public static void main(String[] args) {
+        Olaf olaf = new Olaf();
+        olaf.start();
+    }
+
+    public Olaf() {
+        EPServiceProvider epServiceProvider = EPServiceProviderManager.getDefaultProvider();
+        partnersNotificationService = new PartnersNotificationService(ipAddress2SiteMap);
+        ipAddressUsageNotificationService = new IpAddressUsageNotificationService(epServiceProvider);
+
+        epServiceProvider //
+                .getEPAdministrator() //
+                .createEPL(
+                    "create window RatedIpAddressWindow.win:time(360 min) as " + "select * from "
+                            + RatedIpAddress.class.getName());
+
+        epServiceProvider //
+                .getEPAdministrator() //
+                .createEPL("insert into RatedIpAddressWindow select * from " + RatedIpAddress.class.getName());
+
+        /*
+         * listen to ip address rated events
+         */
+        // IpAddressRatedEventListener.register(epServiceProvider, partnersNotificationService);
+
+        /*
+         * Listen to ip usage in different countries events
+         */
+        IpAddressUsedInDifferentCountriesEventListener.register(epServiceProvider);
+
+        /*
+         * Listen to fraud announcments
+         */
+        IpAddressAnnouncedAsFraudEventListener.register(epServiceProvider);
+
+        /*
+         * Listen to contact events in different countries
+         */
+        IpAddressUsedForContactInDifferentCountriesEventListener.register(epServiceProvider);
+
+        /*
+         * used excessively
+         */
+        IpAddressUsedForContactExcessivelyEventListener.register(epServiceProvider);
+
+        /*
+         * used for posting in different countries
+         */
+        IpAddressUsedForPostingInDifferentCountriesEventListener.register(epServiceProvider);
+    }
+
+    /**
+     * Starts the service.
+     * 
+     * @throws IOException
+     */
+    public void start() {
+        server = new OlafTcpServer(5555, ipAddressUsageNotificationService);
+        websocketServer = new OlafWebsocketServer();
+    }
+
+    public void stop() {
+        server.close();
+        websocketServer.close();
+        partnersNotificationService.shutdown();
+    }
 
 }
---
+++ server/src/main/java/de/mobile/olaf/server/OlafWebsocketServer.java
@@ -0,0 +1,205 @@
+package de.mobile.olaf.server;
+
+import static org.jboss.netty.channel.Channels.pipeline;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.isKeepAlive;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.setContentLength;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.ORIGIN;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.SEC_WEBSOCKET_KEY1;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.SEC_WEBSOCKET_KEY2;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.SEC_WEBSOCKET_LOCATION;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.SEC_WEBSOCKET_ORIGIN;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.SEC_WEBSOCKET_PROTOCOL;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.WEBSOCKET_LOCATION;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.WEBSOCKET_ORIGIN;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.WEBSOCKET_PROTOCOL;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Values.WEBSOCKET;
+import static org.jboss.netty.handler.codec.http.HttpMethod.GET;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.FORBIDDEN;
+import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+import java.net.InetSocketAddress;
+import java.security.MessageDigest;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpHeaders.Names;
+import org.jboss.netty.handler.codec.http.HttpHeaders.Values;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.handler.codec.http.websocket.DefaultWebSocketFrame;
+import org.jboss.netty.handler.codec.http.websocket.WebSocketFrame;
+import org.jboss.netty.handler.codec.http.websocket.WebSocketFrameDecoder;
+import org.jboss.netty.handler.codec.http.websocket.WebSocketFrameEncoder;
+import org.jboss.netty.util.CharsetUtil;
+
+
+/**
+ * Call Websocket server by: ws://localhost:8012/websocket
+ * 
+ * @author hhammel
+ * 
+ */
+public class OlafWebsocketServer extends SimpleChannelUpstreamHandler {
+
+    private final Channel channel;
+
+    private static final String WEBSOCKET_PATH = "/websocket";
+
+    // public static void main(String[] args) {
+    //
+    // OlafWebsocketServer ws = new OlafWebsocketServer();
+    //
+    // }
+
+    public void close() {
+        channel.close().awaitUninterruptibly(30, TimeUnit.SECONDS);
+    }
+
+    public OlafWebsocketServer() {
+
+        // Configure the server.
+        ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
+                Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
+
+        // Create a default pipeline implementation.
+        ChannelPipeline pipeline = pipeline();
+        pipeline.addLast("decoder", new HttpRequestDecoder());
+        pipeline.addLast("aggregator", new HttpChunkAggregator(65536));
+        pipeline.addLast("encoder", new HttpResponseEncoder());
+        pipeline.addLast("handler", this);
+
+        // Set up the event pipeline.
+        bootstrap.setPipeline(pipeline);
+
+        // Bind and start to accept incoming connections.
+        channel = bootstrap.bind(new InetSocketAddress(8014));
+
+    }
+
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+        Object msg = e.getMessage();
+        if (msg instanceof HttpRequest) {
+            handleHttpRequest(ctx, (HttpRequest) msg);
+        } else if (msg instanceof WebSocketFrame) {
+            handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+        }
+    }
+
+    private void handleHttpRequest(ChannelHandlerContext ctx, HttpRequest req) throws Exception {
+        // Allow only GET methods.
+        if (req.getMethod() != GET) {
+            sendHttpResponse(ctx, req, new DefaultHttpResponse(HTTP_1_1, FORBIDDEN));
+            return;
+        }
+
+        // Serve the WebSocket handshake request.
+        if (req.getUri().equals(WEBSOCKET_PATH) && Values.UPGRADE.equalsIgnoreCase(req.getHeader(CONNECTION))
+                && WEBSOCKET.equalsIgnoreCase(req.getHeader(Names.UPGRADE))) {
+
+            // Create the WebSocket handshake response.
+            HttpResponse res = new DefaultHttpResponse(HTTP_1_1, new HttpResponseStatus(101,
+                    "Web Socket Protocol Handshake"));
+            res.addHeader(Names.UPGRADE, WEBSOCKET);
+            res.addHeader(CONNECTION, Values.UPGRADE);
+
+            // Fill in the headers and contents depending on handshake method.
+            if (req.containsHeader(SEC_WEBSOCKET_KEY1) && req.containsHeader(SEC_WEBSOCKET_KEY2)) {
+                // New handshake method with a challenge:
+                res.addHeader(SEC_WEBSOCKET_ORIGIN, req.getHeader(ORIGIN));
+                res.addHeader(SEC_WEBSOCKET_LOCATION, getWebSocketLocation(req));
+                String protocol = req.getHeader(SEC_WEBSOCKET_PROTOCOL);
+                if (protocol != null) {
+                    res.addHeader(SEC_WEBSOCKET_PROTOCOL, protocol);
+                }
+
+                // Calculate the answer of the challenge.
+                String key1 = req.getHeader(SEC_WEBSOCKET_KEY1);
+                String key2 = req.getHeader(SEC_WEBSOCKET_KEY2);
+                int a = (int) (Long.parseLong(key1.replaceAll("[^0-9]", "")) / key1.replaceAll("[^ ]", "").length());
+                int b = (int) (Long.parseLong(key2.replaceAll("[^0-9]", "")) / key2.replaceAll("[^ ]", "").length());
+                long c = req.getContent().readLong();
+                ChannelBuffer input = ChannelBuffers.buffer(16);
+                input.writeInt(a);
+                input.writeInt(b);
+                input.writeLong(c);
+                ChannelBuffer output = ChannelBuffers.wrappedBuffer(MessageDigest.getInstance("MD5").digest(
+                    input.array()));
+                res.setContent(output);
+            } else {
+                // Old handshake method with no challenge:
+                res.addHeader(WEBSOCKET_ORIGIN, req.getHeader(ORIGIN));
+                res.addHeader(WEBSOCKET_LOCATION, getWebSocketLocation(req));
+                String protocol = req.getHeader(WEBSOCKET_PROTOCOL);
+                if (protocol != null) {
+                    res.addHeader(WEBSOCKET_PROTOCOL, protocol);
+                }
+            }
+
+            // Upgrade the connection and send the handshake response.
+            ChannelPipeline p = ctx.getChannel().getPipeline();
+            p.remove("aggregator");
+            p.replace("decoder", "wsdecoder", new WebSocketFrameDecoder());
+
+            ctx.getChannel().write(res);
+
+            p.replace("encoder", "wsencoder", new WebSocketFrameEncoder());
+            return;
+        }
+
+        // Send an error page otherwise.
+        sendHttpResponse(ctx, req, new DefaultHttpResponse(HTTP_1_1, FORBIDDEN));
+    }
+
+    // ****************************************
+    // TODO: NOW ONLY ECHO + UPPERCASE, send sensefull events :)
+    // ****************************************
+
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        // Send the uppercased string back.
+        ctx.getChannel().write(new DefaultWebSocketFrame(frame.getTextData().toUpperCase()));
+    }
+
+    private void sendHttpResponse(ChannelHandlerContext ctx, HttpRequest req, HttpResponse res) {
+        // Generate an error page if response status code is not OK (200).
+        if (res.getStatus().getCode() != 200) {
+            res.setContent(ChannelBuffers.copiedBuffer(res.getStatus().toString(), CharsetUtil.UTF_8));
+            setContentLength(res, res.getContent().readableBytes());
+        }
+
+        // Send the response and close the connection if necessary.
+        ChannelFuture f = ctx.getChannel().write(res);
+        if (!isKeepAlive(req) || res.getStatus().getCode() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+        e.getCause().printStackTrace();
+        e.getChannel().close();
+    }
+
+    private String getWebSocketLocation(HttpRequest req) {
+        return "ws://" + req.getHeader(HttpHeaders.Names.HOST) + WEBSOCKET_PATH;
+    }
+}
